126c307b9399ed29db1f441f4fbc0858bca36597
Kyle McKenna <mckennakyler@gmail.com>
2020-01-27 22:45:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,13 @@
 class Dog 
   attr_accessor :name 
+  # def name=(dog_name)
+  #   @name = dog_name
+  # end   
+  
+  # def name
+  #   @name 
+  # end   
+  
   def bark
     puts "woof!"
   end 
